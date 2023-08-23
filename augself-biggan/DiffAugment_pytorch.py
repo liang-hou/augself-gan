9f43a73412459cd84a10e550295e18c02a5a2d3a
houliang05 <liang-hou@outlook.com>
2023-08-23 20:40:11 +0800

    Update DiffAugment for BigGAN

--- augself-biggan/DiffAugment_pytorch.py
+++ augself-biggan/DiffAugment_pytorch.py
@@ -2,7 +2,6 @@
 # Shengyu Zhao, Zhijian Liu, Ji Lin, Jun-Yan Zhu, and Song Han
 # https://arxiv.org/pdf/2006.10738
 
-import numpy as np
 import torch
 import torch.nn.functional as F
 
@@ -81,43 +80,6 @@ def rand_cutout(x, config):
     mask[grid_batch, grid_x, grid_y] = 0
     x = x * mask.unsqueeze(1)
     return x, torch.cat([offset_x.view(-1, 1).float() / (x.size(2) - cutout_size[0] % 2), offset_y.view(-1, 1).float() / (x.size(3) - cutout_size[1] % 2)], 1)
-
-
-def rand_cutout_test(x, low_ratio=0.25, high_ratio=0.5):
-    cutout_sizes = int(x.size(2) * high_ratio + 0.5), int(x.size(3) * high_ratio + 0.5)
-    cutout_size_x = torch.randint(int(x.size(2) * low_ratio + 0.5), int(x.size(2) * high_ratio + 0.5) + 1, size=[x.size(0), 1, 1], device=x.device)
-    cutout_size_y = torch.randint(int(x.size(3) * low_ratio + 0.5), int(x.size(3) * high_ratio + 0.5) + 1, size=[x.size(0), 1, 1], device=x.device)
-    x_size_2 = torch.where(cutout_size_x % 2 == 0, torch.tensor(x.size(2) + 1, device=x.device), torch.tensor(x.size(2), device=x.device))
-    x_size_3 = torch.where(cutout_size_y % 2 == 0, torch.tensor(x.size(3) + 1, device=x.device), torch.tensor(x.size(3), device=x.device))
-    offset_x = (torch.rand(size=[x.size(0), 1, 1], device=x.device) * x_size_2).long()
-    offset_y = (torch.rand(size=[x.size(0), 1, 1], device=x.device) * x_size_3).long()
-
-    grid_batch, grid_x, grid_y = torch.meshgrid(
-        torch.arange(x.size(0), dtype=torch.long, device=x.device),
-        torch.arange((-cutout_sizes[0]+1) // 2, (cutout_sizes[0]+1) // 2, dtype=torch.long, device=x.device),
-        torch.arange((-cutout_sizes[1]+1) // 2, (cutout_sizes[0]+1) // 2, dtype=torch.long, device=x.device),
-    )
-
-    # grid_x = torch.clamp(grid_x, min=-(cutout_size_x // 2), max=(cutout_size_x - 1) // 2)
-    # grid_y = torch.clamp(grid_y, min=-(cutout_size_y // 2), max=(cutout_size_y - 1) // 2)
-    grid_x_np = grid_x.cpu().numpy()
-    grid_y_np = grid_y.cpu().numpy()
-    cutout_size_x_np = cutout_size_x.cpu().numpy()
-    cutout_size_y_np = cutout_size_y.cpu().numpy()
-    grid_x_np_clip = np.clip(grid_x_np, a_min=-(cutout_size_x_np // 2), a_max=(cutout_size_x_np - 1) // 2)
-    grid_y_np_clip = np.clip(grid_y_np, a_min=-(cutout_size_y_np // 2), a_max=(cutout_size_y_np - 1) // 2)
-    grid_x = torch.from_numpy(grid_x_np_clip).to(x.device)
-    grid_y = torch.from_numpy(grid_y_np_clip).to(x.device)
-    grid_x = torch.clamp(grid_x + offset_x, min=0, max=x.size(2) - 1)
-    grid_y = torch.clamp(grid_y + offset_y, min=0, max=x.size(3) - 1)
-    mask = torch.ones(x.size(0), x.size(2), x.size(3), dtype=x.dtype, device=x.device)
-    mask[grid_batch, grid_x, grid_y] = 0
-    x = x * mask.unsqueeze(1)
-    return x, torch.cat([
-        offset_x.view(-1, 1).float() / (x_size_2.view(-1, 1) - 1), 
-        offset_y.view(-1, 1).float() / (x_size_3.view(-1, 1) - 1),
-        (cutout_size_x.view(-1, 1).float() - int(x.size(2) * low_ratio + 0.5)) / (int(x.size(2) * high_ratio + 0.5) - int(x.size(2) * low_ratio + 0.5)),
-        (cutout_size_y.view(-1, 1).float() - int(x.size(3) * low_ratio + 0.5)) / (int(x.size(3) * high_ratio + 0.5) - int(x.size(3) * low_ratio + 0.5))], 1)
 
 
 AUGMENT_FNS = {
